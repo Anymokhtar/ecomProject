3aa84d262a8c5ee04d82070a5d5cee56325ad964
khalilozich <rqiba.khalil@gmail.com>
2023-05-10 00:00:33 +0100

    paiement

--- ecomProject/ecomApp/urls.py
+++ ecomProject/ecomApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import accueil, recherche_article, profil, CustomLogoutView, UtilisateurCreationView, modifier_profil, \
-    modifier_panier, modifier_article, ajouter_article
-from .views import voir_panier, ajouter_panier, supprimer_panier
+    modifier_panier, modifier_article, ajouter_article, CreateCheckoutSessionView, SuccessView, cancelView, StripeIntentView, stripe_webhook
+from .views import voir_panier, ajouter_panier, supprimer_panier, article_detail
 from .views import CustomLoginView
 from . import views
 urlpatterns = [
@@ -21,6 +21,10 @@ urlpatterns = [
     path('modifier_article/<int:id>/', modifier_article, name='modifier_article'),
     path('articles/<int:article_id>/supprimer/', views.supprimer_article, name='supprimer_article'),
     path('ajouter_article/', ajouter_article, name='ajouter_article'),
-
-
+    path('create_checkout_session/<pk>/', CreateCheckoutSessionView.as_view(), name='create_checkout_session'),
+    path('cancel/', cancelView, name='cancel'),
+    path('success/', SuccessView, name='success'),
+    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
+    path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
+    path('article_detail/<int:id>/', article_detail, name='article_dettail')
 ]
--- ecomProject/ecomApp/views.py
+++ ecomProject/ecomApp/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 import requests
 from bs4 import BeautifulSoup
 from django.core.files.base import ContentFile
@@ -18,21 +19,157 @@ from urllib.request import urlopen
 from .models import Article
 from django.http import request
 from .models import Panier
+from django.conf import settings
+from django.http import JsonResponse
+from django.views import View
+
+stripe_api_key = settings.STRIPE_SECRET_KEY
+
+
+def SuccessView(request):
+    return render(request, 'succes.html')
+
+
+def cancelView(request):
+    return render(request, 'cancelled.html')
+
+
+class StripeIntentView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            req_json = json.loads(request.body)
+            customer = stripe.Customer.create(email=req_json['email'])
+            article_id = self.kwargs["pk"]
+            article = Article.objects.get(id=article_id)
+            intent = stripe.PaymentIntent.create(
+                amount=article.prix,
+                currency='usd',
+                customer=customer['id'],
+                metadata={
+                    "article_id": article.id
+                }
+            )
+            return JsonResponse({
+                'clientSecret': intent['client_secret']
+            })
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+
+
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        article_id = self.kwargs["pk"]
+        article = get_object_or_404(Article, id=article_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': article.price,  # Assuming the price field is called "price"
+                    'name': article.title,  # Assuming the title field is called "title"
+                    'quantity': 1,  # Assuming a quantity of 1 for now
+                },
+            ],
+            metadata={
+                "article_id": article.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        customer_email = session["customer_details"]["email"]
+        product_id = session["metadata"]["article_id"]
+
+        article = Article.objects.get(id=article_id)
+
+        send_mail(
+            subject="Here is your product",
+            message=f"Thanks for your purchase. Here is the product you ordered.",
+            recipient_list=[customer_email],
+            from_email="matt@test.com"
+        )
+
+        # TODO - decide whether you want to send the file or the URL
+
+    elif event["type"] == "payment_intent.succeeded":
+        intent = event['data']['object']
+
+        stripe_customer_id = intent["customer"]
+        stripe_customer = stripe.Customer.retrieve(stripe_customer_id)
+
+        customer_email = stripe_customer['email']
+        article_id = intent["metadata"]["article_id"]
+
+        article = Article.objects.get(id=article_id)
+
+        send_mail(
+            subject="Here is your product",
+            message=f"Thanks for your purchase. Here is the product you ordered.",
+            recipient_list=[customer_email],
+            from_email="matt@test.com"
+        )
+
+    return HttpResponse(status=200)
 
 
 def accueil(request):
     articles = Article.objects.all()[:]
-    context = {'articles': articles}
+    context = {'articles': articles,
+               'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
+               }
+
     return render(request, 'accueil.html', context)
 
 
+#def recherche_article(request):
+   # query = request.GET.get('q')
+   # articles = None
+   # if query:
+     #   articles = Article.objects.filter(titre__icontains=query)
+   # context = {'articles': articles, 'query': query}
+   # return render(request, 'recherche_article.html', context)
 def recherche_article(request):
-    query = request.GET.get('q')
-    articles = None
+    query = request.GET.get('q')  # Récupérer le terme de recherche depuis les paramètres de requête
+
+    articles = Article.objects.all()  # Récupérer tous les articles
+
     if query:
-        articles = Article.objects.filter(titre__icontains=query)
-    context = {'articles': articles, 'query': query}
+        # Si un terme de recherche est spécifié, filtrer les articles en fonction du titre ou de la description
+        articles = articles.filter(titre__icontains=query) | articles.filter(description__icontains=query) | articles.filter(prix__icontains=query)
+
+    context = {
+        'articles': articles,
+        'query': query,
+    }
+
     return render(request, 'recherche_article.html', context)
+
 
 
 # Vue de connexion personnalisée
@@ -92,6 +229,18 @@ def modifier_article(request, id):
     context = {'form': form}
     return render(request, 'modifier_article.html', context)
 
+#def article_detail(request, id):
+   # article = get_object_or_404(Article, id=id, vendeur=request.user)
+   # if request.method == 'POST':
+    #    form = ArticleForm(request.POST, request.FILES, instance=article)
+  #  else:
+   #     form = ArticleForm(instance=article)
+   # context = {'form': form}
+   # return render(request, 'article_dettail.html', context)
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    context = {'article': article}
+    return render(request, 'article_dettail.html', context)
 
 @login_required
 def ajouter_article(request):
